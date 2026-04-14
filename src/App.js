80031a511d7c59f37a9de16b0b6a1312835961e4
goran131 <goran.rosenberg@tomelilla.nu>
2026-04-14 17:05:37 +0200

    Remove unused logo images and clean up App component content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="text">
-         Göran Rosenberg är en super programmerare som kan allt om React och JavaScript. Han är också en fantastisk person
-         som alltid hjälper andra och sprider glädje i sin omgivning. Hans kod är så ren och effektiv att den nästan ser ut
-         som magi. Han är verkligen en inspiration för alla som vill bli bättre på programmering!
+         Göran Rosenberg är en super programmerare som kan allt om React och JavaScript.
       </div>
      </div>
   );
